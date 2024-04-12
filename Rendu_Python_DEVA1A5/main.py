eb5b34ec238a6859faecf367362ac8cc5bb0d486
ovsiankina <ovsiankina@proton.me>
2024-04-12 11:53:09 +0200

    MaJ at orif

--- Rendu_Python_DEVA1A5/main.py
+++ Rendu_Python_DEVA1A5/main.py
@@ -6,27 +6,63 @@ parser = argparse.ArgumentParser(description='description : argparse test')
 parser.add_argument('fichierEntree',
                     help="Definit le fichier d'entree qui sera chiffre ou dechiffre")
 
-parser.add_argument('Cle',
+parser.add_argument('cle',
                     help="Definit la cle qui sera utilisee pour chiffrer ou dechiffrer")
 
 parser.add_argument('fichierSortie',
                     help="Definit le fichier de sortie ou le resultat sera stocke")
 
 # Optional arguments
-parser.add_argument('-c',
+group = parser.add_mutually_exclusive_group()
+
+group.add_argument('-c',
                     '--chiffrement',
                     action="store_true",
-                    required=False,
                     help="Permet de specifier l'action de chiffrement")
 
-parser.add_argument('-d',
+group.add_argument('-d',
                     '--dechiffrement',
-                    type=str,
-                    required=False,
+                    action="store_true",
                     help="Permet de specifier l'action de dechiffrement")
 
-def Vigenere (args.fichierEntree,args.Cle,args.fichierSortie)
-    
+def Chiffrement(fichierEntree, cle, fichierSortie, alphabet):
+    print(f"{fichierEntree}, {cle}, {fichierSortie}")
+
+    i = 0
+    j = 0
+    a = alphabet
+
+    parsedKey = [characters for characters in cle]
+
+    notEncryptedFile = open(fichierEntree, 'r')
+    notEncryptedFileString = notEncryptedFile.read()
+
+    encryptedFile = open(fichierSortie, 'a')
+
+    for character in notEncryptedFileString:
+
+        if j >= len(parsedKey):
+            j = 0
+
+        positionInput = a.index(character) 
+        positionKey = a.index(parsedKey[j]) 
+        
+        # TexteChiffré[i] = (TexteClaire[i] + Clés[i]) modulo 26
+        encryptedCharacter = (positionInput + positionKey)%26 # position of key 
+
+        print(f"""
+                {character} + {parsedKey[j]} => {a[encryptedCharacter]}
+                key index = {positionKey}
+            """)
+
+        encryptedFile.write(a[encryptedCharacter])
+        
+        i += 1
+        j += 1
+
+
+def Dechiffrement(fichierEntree, cle, fichierSortie, alphabet):
+    print(f"{fichierEntree}, {cle}, {fichierSortie}, {alphabet}")
 
 if __name__ == "__main__":
     print("""
@@ -35,13 +71,22 @@ if __name__ == "__main__":
     -------------
     """)
 
+    alphabet = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    ]
+
     args = parser.parse_args()
+    print(args)
+
+    # What happens is created but then empty and try decrypt?
+    if not os.path.exists(args.fichierEntree):
+        create = open(args.fichierEntree, 'x')
+        create.close()
 
     if args.chiffrement:
-        print(f'cle: {args.Cle}')
-
-        inputText = open(args.fichierEntree)
-        print(inputText.read())
-
-    if args.dechiffrement:
-        print(f'Dechiffrement value: {args.dechiffrement}')
+        Chiffrement(args.fichierEntree,args.cle,args.fichierSortie,alphabet)
+    elif args.dechiffrement:
+        Dechiffrement(args.fichierEntree,args.cle,args.fichierSortie,alphabet)
+    else:
+        print("Choisissez -c ou -d")
