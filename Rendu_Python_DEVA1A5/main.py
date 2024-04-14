8ea8f87aa7652dc119b08327a2eb3af04feb8a89
Ovsiankina <ovsiankina@proton.me>
2024-04-14 17:20:03 +0200

    Code refactor. No duplicate code. -c and -d have been separated

--- Rendu_Python_DEVA1A5/main.py
+++ Rendu_Python_DEVA1A5/main.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser(description='description : argparse test')
 parser.add_argument('fichierEntree',
                     help="Definit le fichier d'entree qui sera chiffre ou dechiffre")
 
-parser.add_argument('cle',
+parser.add_argument('Cle',
                     help="Definit la cle qui sera utilisee pour chiffrer ou dechiffrer")
 
 parser.add_argument('fichierSortie',
@@ -30,132 +30,13 @@ parser.add_argument('-v',
                     action="store_true",
                     help="Description de ce qu'il ce passe")
 
-def Chiffrement(fichierEntree, cle, fichierSortie, alphabet):
+def Chiffrement(characterIndex, keyIndex):
+    # TexteChiffré[i] = (TexteClaire[i] + Clés[i]) modulo 26
+   return (characterIndex + keyIndex)%26 # position of key 
 
-    i = 0
-    j = 0
-    a = alphabet
-
-    keyString = [characters for characters in cle]
-
-    inputFile = open(fichierEntree, 'r')
-    inputString = inputFile.read()
-    outputFile = open(fichierSortie, 'w')
-
-    for character in inputString:
-
-        if j >= len(keyString):
-            j = 0
-
-        """
-            Check if the current character is alphabetic
-
-            If yes, it'll convert both the current key and character into
-            the correct alphabet[indexes] and do calculations with these indexes.
-
-            Then converts back the alphabet[index] into a letter which can be
-            written into the output file.
-        """
-        if character.isalpha():
-            characterIndex = a.index(character.lower())
-            keyIndex = a.index(keyString[j]) 
-            
-            # TexteChiffré[i] = (TexteClaire[i] + Clés[i]) modulo 26
-            outputCharacter = (characterIndex + keyIndex)%26 # position of key 
-
-            if args.verbose:
-                # Prevent trailing whitespace when printing
-                verbose = f"""
-                                === VERBOSE ===
-                                char:{character} + key:{keyString[j]} => {a[outputCharacter]}.
-                                key index = {keyIndex}
-                        """
-                print(verbose)
-
-            ## Check if character is caps
-            if not character.isupper():
-                outputFile.write(a[outputCharacter])
-            else:
-                print(f"{character} is upper")
-                print("")
-                outputFile.write(a[outputCharacter].upper())
-
-            # j is here to prevent the key from "moving" when special characters
-            j += 1
-
-        elif not character.isalpha():
-            if args.verbose:
-                print(f"""
-                                '{character}' is not alpha
-                                NO CHANGES
-                """)
-            outputFile.write(character)
-        # Count loop 
-        i += 1
-
-def Dechiffrement(fichierEntree, cle, fichierSortie, alphabet):
-
-    i = 0
-    j = 0
-    a = alphabet
-
-    keyString = [characters for characters in cle]
-
-    inputFile = open(fichierEntree, 'r')
-    inputString = inputFile.read()
-    outputFile = open(fichierSortie, 'w')
-
-    for character in inputString:
-
-        if j >= len(keyString):
-            j = 0
-
-        """
-            Check if the current character is alphabetic
-
-            If yes, it'll convert both the current key and character into
-            the correct alphabet[indexes] and do calculations with these indexes.
-
-            Then converts back the alphabet[index] into a letter which can be
-            written into the output file.
-        """
-        if character.isalpha():
-            characterIndex = a.index(character.lower())
-            keyIndex = a.index(keyString[j]) 
-            
-            # TexteChiffré[i] = (TexteClaire[i] + Clés[i]) modulo 26
-            outputCharacter = (characterIndex - keyIndex)%26 # position of key 
-            # TexteClaire[i] = (TexteChiffré[i] - Clés[i]) modulo 26
-
-            if args.verbose:
-                # Prevent trailing whitespace when printing
-                verbose = f"""
-                                === VERBOSE ===
-                                char:{character} + key:{keyString[j]} => {a[outputCharacter]}.
-                                key index = {keyIndex}
-                        """
-                print(verbose)
-
-            ## Check if character is caps
-            if not character.isupper():
-                outputFile.write(a[outputCharacter])
-            else:
-                print(f"{character} is upper")
-                print("")
-                outputFile.write(a[outputCharacter].upper())
-
-            # j is here to prevent the key from "moving" when special characters
-            j += 1
-
-        elif not character.isalpha():
-            if args.verbose:
-                print(f"""
-                                '{character}' is not alpha
-                                NO CHANGES
-                """)
-            outputFile.write(character)
-        # Count loop 
-        i += 1
+def Dechiffrement(characterIndex, keyIndex):
+    # TexteClaire[i] = (TexteChiffré[i] - Clés[i]) modulo 26
+    return (characterIndex - keyIndex)%26 # position of key 
 
 if __name__ == "__main__":
     print("""
@@ -164,24 +45,95 @@ if __name__ == "__main__":
     -------------
     """)
 
-    alphabet = [
+    # Alphabet used in the code
+    a = [
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
         'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
     ]
 
     args = parser.parse_args()
-    print(args)
-
+    
     # What happens is created but then empty and try decrypt?
+
+    # Check if input file exists
     if not os.path.exists(args.fichierEntree):
         create = open(args.fichierEntree, 'x')
         create.close()
+    # Open files and get content
+    inputFile = open(args.fichierEntree, 'r')
+    inputString = inputFile.read()
 
-    if args.chiffrement:
-        Chiffrement(args.fichierEntree,args.cle,args.fichierSortie,alphabet)
+    key = args.Cle
+    keyString = [characters for characters in key]
 
-    elif args.dechiffrement:
-        Dechiffrement(args.fichierEntree,args.cle,args.fichierSortie,alphabet)
-    else:
-        print("Choisissez -c ou -d")
-        # force le -h pour expliquer comment utiliser la commande
+    outputFile = open(args.fichierSortie, 'w')
+    outputString = ''
+    outputCharacterIndex = 0
+
+    i = 0
+    j = 0
+
+    for character in inputString:
+
+        if j >= len(keyString):
+            j = 0
+
+        """
+            Check if the current character is alphabetic
+
+            If yes, it'll convert both the current key and character into
+            the correct alphabet[indexes] and do calculations with these indexes.
+
+            Then converts back the alphabet[index] into a letter which can be
+            written into the output file.
+        """
+        if character.isalpha():
+            characterIndex = a.index(character.lower())
+            keyIndex = a.index(keyString[j]) 
+
+            if args.chiffrement:
+                outputCharacterIndex = int(Chiffrement(characterIndex, keyIndex))
+            elif args.dechiffrement:
+                outputCharacterIndex = int(Dechiffrement(characterIndex, keyIndex))
+            else:
+                print("Choisissez -c ou -d")
+                # force le -h pour expliquer comment utiliser la commande
+
+            if args.verbose:
+                # Prevent trailing whitespace when printing
+                verbose = f"""
+                                === VERBOSE ===
+                                char:{character} + key:{keyString[j]} => {a[outputCharacterIndex]}.
+                                key index = {keyIndex}
+                        """
+                print(verbose)
+
+            ## Check if character is caps
+            if not character.isupper():
+                print(outputString)
+                outputString = outputString + a[outputCharacterIndex]
+                print(outputString)
+            else:
+                outputString = outputString + a[outputCharacterIndex].upper()
+                print(outputString)
+
+            # j is here to prevent the key from "moving" when special characters
+            j += 1
+
+        elif not character.isalpha():
+            if args.verbose:
+                print(f"""
+                                '{character}' is not alpha
+                                NO CHANGES
+                """)
+
+            outputString = outputString + character
+        # Count loop 
+        i += 1
+
+    # Write the output into the output file
+    print(outputString)
+    outputFile.write(outputString)
+    # End of program. Closing files
+    inputFile.close()
+    outputFile.close()
